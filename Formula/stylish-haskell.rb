397c51930ba6c0618119d4956c7aa999cef6f6a7
Mizunashi Mana <mizunashi-mana@users.noreply.github.com>
2020-08-10 21:49:26 +0900

    Update checksum

--- Formula/stylish-haskell.rb
+++ Formula/stylish-haskell.rb
@@ -2,7 +2,7 @@ class StylishHaskell < Formula
   desc "A Haskell code prettifier."
   homepage "https://github.com/jaspervdj/stylish-haskell"
   url "https://github.com/jaspervdj/stylish-haskell/archive/v0.11.0.3.tar.gz"
-  sha256 "661791f67681268ea20243a2b771d40f59122ee7a88bd7ff4a34231bc3206ca5"
+  sha256 "9581e4146490dff7594d2c31bf4610b302dacf617b8b83b063422206a68b9f2c"
   license "BSD3"
   head "https://github.com/jaspervdj/stylish-haskell.git"
 
